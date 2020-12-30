aa1da7368d75578c6349c30fb6dee2bd8d90c8cf
Jawad Jaber <jawadsjaber@outlook.com>
2020-12-30 14:58:10 +0300

    Add project files.

---
+++ Client/wwwroot/htmltopdf.js
@@ -0,0 +1,13 @@
+﻿window.htmltopdf = (d) =>
+{
+    var element = document.getElementById(d);
+    var opt = {
+        margin: 1,
+        filename: 'myfile2.pdf',
+        image: { type: 'png', quality: 1 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+    };
+    html2pdf(element,opt);
+    
+}
